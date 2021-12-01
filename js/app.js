5f2443012f532d44be0cd2f546c326dcc0aacc05
Cristian Mazzetti Martin <cristian@kalexgroup.ltd>
2021-12-01 15:45:20 +0000

    update

--- js/app.js
+++
@@ -1,9 +0,0 @@
-const RootComponent = {
-    data() {
-        return {
-          message: 'Hello World!'
-        }
-    }
-}
-const app = Vue.createApp(RootComponent)
-const vm = app.mount('#app')
