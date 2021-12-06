3866e835ae0e8f4f7eea2177fa2cc4538cf0aeea
Cristian Mazzetti Martin <cristian@kalexgroup.ltd>
2021-12-06 14:53:14 +0000

    update

---
+++ js/app.js
@@ -0,0 +1,12 @@
+// Wait for the page to load first
+window.onload = function() {
+
+    // MAP
+    var navMap = document.getElementById("navMap");
+    navMap.onclick = function() {
+        // CODE
+        return false;
+    }
+
+    
+}
