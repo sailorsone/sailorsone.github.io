1c3d492ef2ec487dead1d43919081bea265e7772
Cristian Mazzetti Martin <cristian@kalexgroup.ltd>
2021-12-14 13:33:06 +0000

    update loadContent

--- js/app.js
+++ js/app.js
@@ -5,6 +5,9 @@ function loadContent(url) {
         return response.text();
     }).then(function (data) {
         document.querySelector('#main').innerHTML = data;
+        document.querySelector("#main").querySelectorAll('script').forEach(function (pageScript) {
+            eval(pageScript.innerText);
+        });
     }).catch(function (err) {
         console.log('Errore: ', err);
     });
