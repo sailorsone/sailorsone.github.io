8fb9158e67e0bed978a5417f9c116b4a0718d2f1
Cristian Mazzetti Martin <cristian@kalexgroup.ltd>
2021-12-16 16:15:25 +0000

    delete getJSON

--- js/app.js
+++ js/app.js
@@ -12,21 +12,3 @@ function loadContent(url) {
         console.log('Errore: ', err);
     });
 }
-
-function getJSON(url) {
-    var request = new XMLHttpRequest();
-    request.open('GET', url, true);
-
-    request.onload = function() {
-        if (request.status >= 200 && request.status < 400) {
-            // Success!
-            var data = JSON.parse(request.responseText);
-        } else {
-            // We reached our target server, but it returned an error
-        }
-    };
-    request.onerror = function() {
-        // There was a connection error of some sort
-    };
-    request.send();
-}
